cc3a7bdf8c4689a6dbe95b45b9f341b58bbebb5d
t0rpid <39504226+t0rpid@users.noreply.github.com>
2018-11-29 17:07:59 -0500

    Delete gfn_patch.cmd

--- gfn_patch.cmd
+++
@@ -1,4 +0,0 @@
-title GeForce NOW Patch Tool v1 by Torpid
-cd %localappdata%\NVIDIA Corporation\GeForceNOW\CEF
-ren "GeForceNOW.exe" "GeForceNOW.bat"
-explorer "%localappdata%\NVIDIA Corporation\GeForceNOW\CEF\GeForceNOW.bat"
